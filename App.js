c5a0164b113c593c9f04742bc4cfe7cd1c8724ce
AUNGMYATLWIN <zack.cloud7@gmail.com>
2023-03-09 01:07:57 +0630

    removed react import

--- App.js
+++ App.js
@@ -1,4 +1,3 @@
-import * as React from "react";
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
