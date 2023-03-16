3fcf06943f6563fada0192454015dc85be7dd530
AUNGMYATLWIN <zack.cloud7@gmail.com>
2023-03-17 03:12:40 +0630

    added useeffect to fetch id. and datas from db

--- screens/PlaceDetails.js
+++ screens/PlaceDetails.js
@@ -1,14 +1,19 @@
+import { useEffect } from "react";
 import { Image, ScrollView, StyleSheet, View } from "react-native";
 // import { View } from "react-native-web";
 import OutLineButton from "../components/ui/OutlineButton";
+import { COLORS } from "../constants/colors";
 
-function PlaceDetails() {
+function PlaceDetails({ route }) {
   function showOnMapHandler() {}
+  const selectedPlaceId = route.params.placeId;
+  useEffect(() => {}, [selectedPlaceId]);
   return (
     <ScrollView>
-      <View>
-        <View>
-          <Text>address</Text>
+      <Image style={styles.image} />
+      <View style={styles.locationContainer}>
+        <View style={styles.addressContainer}>
+          <Text style={styles.address}>address</Text>
         </View>
         <OutLineButton icon='map' onPress={showOnMapHandler}>
           View on Map
@@ -20,4 +25,24 @@ function PlaceDetails() {
 
 export default PlaceDetails;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  image: {
+    height: "35%",
+    minHeight: 300,
+    width: "100%",
+  },
+
+  locationContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  addressContainer: {
+    padding: 20,
+  },
+  address: {
+    color: COLORS.primary500,
+    textAlign: "center",
+    fontWeight: "bold",
+    fontSize: 16,
+  },
+});
