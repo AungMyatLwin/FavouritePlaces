054b1388ab22cdce1fc519d31ff7528ef19e05b2
AUNGMYATLWIN <zack.cloud7@gmail.com>
2023-03-17 03:11:09 +0630

    register new screen

--- App.js
+++ App.js
@@ -9,6 +9,7 @@ import Map from "./screens/Map";
 import { useEffect, useState } from "react";
 import { init } from "./util/database";
 import AppLoading from "expo-app-loading";
+import PlaceDetails from "./screens/PlaceDetails";
 
 const Stack = createNativeStackNavigator();
 export default function App() {
@@ -61,6 +62,13 @@ export default function App() {
             component={AddPlace}
             options={{
               title: "Add a new Place",
+            }}
+          />
+          <Stack.Screen
+            name='PlaceDetails'
+            component={PlaceDetails}
+            options={{
+              title: "Detail Place",
             }}
           />
           <Stack.Screen name='Map' component={Map} />
